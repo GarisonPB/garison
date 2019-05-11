13314fccfe4abcc6d41cde6d566b0ca2a9880aa6
GarisonPB <muhammadgarison99@gmail.com>
2019-05-11 22:42:51 +0700

    update to deploy to master

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 
-const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/heroicon-app'));
 
 app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/index.html');
+  res.status(200).sendFile(__dirname + '/heroicon-app/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
